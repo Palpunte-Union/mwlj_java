ea22852358df38cf4e38f461815c91be05b38a8f
eight_y_88 <jadec.eight@gmail.com>
2021-08-29 09:01:54 +0900

    Player

--- src/main/java/si/f5/pa_union/mwj/instances/players/MWLPlayer.java
+++ src/main/java/si/f5/pa_union/mwj/instances/players/MWLPlayer.java
@@ -1,4 +1,14 @@
 package si.f5.pa_union.mwj.instances.players;
 
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
 public class MWLPlayer {
+    private RoleType role = RoleType.NONE;
+    private UUID uuid;
+
+    public MWLPlayer(Player player) {
+        uuid = player.getUniqueId();
+    }
 }
