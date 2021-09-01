c1cade5625f90fdad2128a609456063d9384b2e6
eight_y_88 <jadec.eight@gmail.com>
2021-09-01 18:15:12 +0900

    roles

--- src/main/java/si/f5/pa_union/mwj/instances/players/MWLPlayer.java
+++ src/main/java/si/f5/pa_union/mwj/instances/players/MWLPlayer.java
@@ -6,14 +6,29 @@ import org.bukkit.entity.Player;
 import java.util.UUID;
 
 public class MWLPlayer {
-    private RoleType role = RoleType.NONE;
+    private RoleType currentRole;
+    private final RoleType prevRole;
     private final UUID uuid;
 
-    public MWLPlayer(Player player) {
+    public MWLPlayer(Player player, RoleType prevRole) {
         uuid = player.getUniqueId();
+        this.prevRole = prevRole;
+        currentRole = prevRole;
     }
     
     public Player getPlayer() {
         return Bukkit.getPlayer(uuid);
+    }
+
+    public RoleType getCurrentRole() {
+        return currentRole;
+    }
+
+    public RoleType getPrevRole() {
+        return prevRole;
+    }
+
+    public void setCurrentRole(RoleType currentRole) {
+        this.currentRole = currentRole;
     }
 }
